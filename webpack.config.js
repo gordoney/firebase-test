690d5077c3947047bd2ef27e69e71e8e5bbbf276
Siarhei Balonikau <siarhei_balonikau@epam.com>
2024-01-31 17:02:48 +0400

    '5'

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const isProduction = process.env.NODE_ENV == 'production';
 
 const stylesHandler = 'style-loader';
 
-console.log('test');
+console.log('test', process.env.CONTENTFUL_TOKEN);
 
 
 const config = {
